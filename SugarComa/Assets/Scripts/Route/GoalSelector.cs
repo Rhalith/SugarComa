c84a86d56a017276229c60ab32396d8fce7186fd
Rhalith <akmannuhyigit@gmail.com>
2022-06-22 17:53:58 +0300

    added camera follow to new goal platform

--- SugarComa/Assets/Scripts/Route/GoalSelector.cs
+++ SugarComa/Assets/Scripts/Route/GoalSelector.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; using Cinemachine;
 
 public class GoalSelector : MonoBehaviour
 {
     [SerializeField] List<Platform> platforms;
 
-    [SerializeField] PlayerMovement _playerMovement;
-    
+    [SerializeField] PlayerMovement _playerMovement; //for current platform
+
+    [SerializeField] CinemachineVirtualCamera _camera;
+
+    [SerializeField] CinemachineBrain _cinemachineBrain;
+
+    private int realPriority;
     public void RandomGoalSelect()
     {
         int i = Random.Range(0, platforms.Count+1);
         if (platforms[i].specification != PlatformSpecification.Goal)
         {
             platforms[i].specification = PlatformSpecification.Goal;
+            VirtualCameraLookTo(_camera, platforms[i].transform);
             print(platforms[i]);
             return;
         }
@@ -21,5 +27,25 @@ public class GoalSelector : MonoBehaviour
         {
             RandomGoalSelect();
         }
+    }
+
+    private void VirtualCameraLookTo(CinemachineVirtualCamera camera, Transform target)
+    {
+        camera.Follow = target;
+        camera.LookAt = target;
+
+         
+        realPriority = camera.Priority;
+
+        CinemachineTransposer transposer = camera.AddCinemachineComponent<CinemachineTransposer>();
+
+        transposer.m_FollowOffset = new Vector3(-9f, 5f, 0f);
+        camera.Priority = 6;
+
+    }
+
+    public void ResetCameraPriority(CinemachineVirtualCamera camera)
+    {
+        camera.Priority = realPriority;
     }
 }
