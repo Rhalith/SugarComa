e99c6cfa8cd61ccf47d9a77f4aa2f1c1d8477f03
Rhalith <akmannuhyigit@gmail.com>
2022-08-04 14:06:45 +0300

    draft of explosion added
    not finished waiting for gamedesigner to clear it

--- SugarComa/Assets/MiniGames/FallingStars/Scripts/Meteors/ExplosionMeteor.cs
+++ SugarComa/Assets/MiniGames/FallingStars/Scripts/Meteors/ExplosionMeteor.cs
@@ -10,11 +10,38 @@ namespace Assets.MiniGames.FallingStars.Scripts.Meteors
         #region Properties
         [SerializeField] int _duration;
         [SerializeField] float _damage;
+        [SerializeField] float _explosionRatio;
+        public bool isPlayerIn;
+        private int _localDuration;
         #endregion
 
         #region OtherComponents
         [SerializeField] Meteor _currentMeteor;
+        [SerializeField] MeteorShadow _currentShadow;
         #endregion
+
+        private void OnEnable()
+        {
+            if (_currentShadow.isPlayerInShadow)
+            {
+                foreach (PlayerSpecs player in _currentShadow._playerList)
+                {
+                    KillPlayer(player);
+                }
+            }
+            _localDuration = _duration;
+            Collider[] colliders = Physics.OverlapSphere(transform.position, 1f);
+
+            foreach (Collider nearby in colliders)
+            {
+                print(nearby);
+                Rigidbody rig = nearby.GetComponent<Rigidbody>();
+                if (rig != null)
+                {
+                    rig.AddExplosionForce(_explosionRatio, transform.position, 5f);
+                }
+            }
+        }
 
         public void DamagePlayer(PlayerSpecs player, float damage)
         {
