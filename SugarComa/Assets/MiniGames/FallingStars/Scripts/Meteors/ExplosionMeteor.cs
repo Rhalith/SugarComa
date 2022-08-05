becb267f9e464c42956badc37eb152a3d46aedc2
Rhalith <akmannuhyigit@gmail.com>
2022-08-05 14:52:37 +0300

    test

--- SugarComa/Assets/MiniGames/FallingStars/Scripts/Meteors/ExplosionMeteor.cs
+++ SugarComa/Assets/MiniGames/FallingStars/Scripts/Meteors/ExplosionMeteor.cs
@@ -1,10 +1,7 @@
-using System;
 using Assets.MiniGames.FallingStars.Scripts.Player;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Windows.WebCam;
-using Random = UnityEngine.Random;
 
 namespace Assets.MiniGames.FallingStars.Scripts.Meteors
 {
@@ -27,7 +24,6 @@ namespace Assets.MiniGames.FallingStars.Scripts.Meteors
         [SerializeField] Meteor _currentMeteor;
         [SerializeField] MeteorShadow _currentShadow;
 
-        [SerializeField] private GameObject _miniMeteor;
         #endregion
 
         private void OnEnable()
@@ -42,6 +38,7 @@ namespace Assets.MiniGames.FallingStars.Scripts.Meteors
             DistributeMeteors();
             _localDuration = _duration;
         }
+
         public void DamagePlayer(PlayerSpecs player, float damage)
         {
             throw new System.NotImplementedException();
@@ -57,7 +54,8 @@ namespace Assets.MiniGames.FallingStars.Scripts.Meteors
         {
             Collider[] colliders = Physics.OverlapSphere(transform.position, 1f);
 
-          foreach (Collider nearby in colliders)
+            //transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+            foreach (Collider nearby in colliders)
             {
                 print(nearby);
                 Rigidbody rig = nearby.GetComponent<Rigidbody>();
@@ -72,40 +70,9 @@ namespace Assets.MiniGames.FallingStars.Scripts.Meteors
                     Vector3 distance = rig.velocity * 1.05f;
                     distance += rig.transform.position;
                     //print("distance:" + distance + "name:" + rig.name);
-                    Instantiate(_currentShadow, new Vector3(distance.x, 0, distance.z), Quaternion.identity,
-                        transform);
+                    Instantiate(_currentShadow, new Vector3(distance.x,0,distance.z), Quaternion.identity);
                 }
             }
-          /*
-          for (int i = 0; i < 3; i++)
-          {
-              Rigidbody rig;
-              
-              GameObject instance = Instantiate(_miniMeteor, getMiniMeteorPos()+transform.position, Quaternion.identity,
-                  transform);
-              rig = instance.GetComponent<Rigidbody>();
-              _explosionDistributionRatio = Random.Range(10, _maxExplosionRatio);
-              // rig.AddExplosionForce(_explosionRatio, transform.position, 5f);
-              rig.AddForce(rig.gameObject.transform.localPosition * _explosionForce, ForceMode.Force);
-              rig.velocity +=
-                  new Vector3(rig.gameObject.transform.localPosition.x, 0,
-                      rig.gameObject.transform.localPosition.z) *_explosionDistributionRatio;
-              Vector3 distance = rig.velocity;
-              distance += rig.transform.position;
-              //print("distance:" + distance + "name:" + rig.name);
-              Instantiate(_currentShadow, new Vector3(distance.x, 0, distance.z), Quaternion.identity,
-                  transform);
-            }*/
-        }
-
-        private Vector3 getMiniMeteorPos()
-        {
-            float x = Random.Range(0f, 1f);
-            float z = MathF.Sqrt(1f - Mathf.Pow(x, 2));
-            Vector3 vector = new Vector3(x,1f,z);
-            
-            print(vector);
-            return vector;
         }
     }
 }
