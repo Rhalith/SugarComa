78bdf06269fd0740c9214b1e68271773a3c9da41
Rhalith <akmannuhyigit@gmail.com>
2022-08-23 13:40:36 +0300

    scene updated

--- SugarComa/Assets/MiniGames/FallingStars/Scripts/Utils/LavaFlow.cs
+++ SugarComa/Assets/MiniGames/FallingStars/Scripts/Utils/LavaFlow.cs
@@ -13,6 +13,7 @@ namespace Assets.MiniGames.FallingStars.Scripts.Utils
         [SerializeField] private float _endValue;
         void Start()
         {
+            _material.SetFloat("_Flow", 0);
             _material.DOFloat(_endValue, "_Flow", _miniGameManager.GameTime);
         }
     }
