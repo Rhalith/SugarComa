9ab500f18509b6a8c84e8487f751d86aeab9403d
Rhalith <81980850+Rhalith@users.noreply.github.com>
2022-08-27 19:19:19 +0300

    new character controller and combat cam

--- SugarComa/Assets/MiniGames/KosKosabilirsen/Scripts/ThirdPerson/PlayerMovement.cs
+++ SugarComa/Assets/MiniGames/KosKosabilirsen/Scripts/ThirdPerson/PlayerMovement.cs
@@ -1,18 +1,75 @@
+using Assets.MiniGames.KosKosabilirsen.Scripts.Player;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
-public class PlayerMovement : MonoBehaviour
+namespace Assets.MiniGames.KosKosabilirsen.Scripts.ThirdPerson
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerMovement : MonoBehaviour
     {
-        
+        [Header("Movement")]
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _groundDrag;
+        [SerializeField] private float _airMultiplier;
+
+        [SerializeField] private Transform _orientation;
+        [SerializeField] private Rigidbody _rb;
+        [SerializeField] private PlayerInputReceiver _playerInputReceiver;
+
+        [Header("Ground Check")]
+        public float playerHeight;
+        public LayerMask whatIsGround;
+        public bool grounded;
+
+        private void FixedUpdate()
+        {
+            MovePlayer();
+        }
+        private void Update()
+        {
+            grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+            SpeedControl();
+
+            if (grounded)
+            {
+                _rb.drag = _groundDrag;
+            }
+            else
+            {
+                _rb.drag = 0;
+            }
+        }
+
+        Vector3 moveDirection;
+        private void MovePlayer()
+        {
+            moveDirection = _orientation.forward * _playerInputReceiver.move.y + _orientation.right * _playerInputReceiver.move.x;
+
+            if (grounded)
+            {
+                _rb.AddForce(moveDirection.normalized * _moveSpeed * 20f, ForceMode.Force);
+            }
+            else
+            {
+                _rb.AddForce(moveDirection.normalized * _moveSpeed * 20f* _airMultiplier, ForceMode.Force);
+            }
+        }
+
+        private void SpeedControl()
+        {
+            Vector3 currentVelocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
+
+            if(currentVelocity.magnitude > _moveSpeed)
+            {
+                Vector3 limitedVelocity = currentVelocity.normalized * _moveSpeed;
+
+                _rb.velocity = new Vector3(limitedVelocity.x, _rb.velocity.y, limitedVelocity.z);
+            }
+        }
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- SugarComa/Assets/MiniGames/KosKosabilirsen/Scripts/ThirdPerson/ThirdPersonCam.cs
+++ SugarComa/Assets/MiniGames/KosKosabilirsen/Scripts/ThirdPerson/ThirdPersonCam.cs
@@ -1,12 +1,58 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonCam : MonoBehaviour
+namespace Assets.MiniGames.KosKosabilirsen.Scripts.ThirdPerson
 {
-    
-    void Update()
+    public class ThirdPersonCam : MonoBehaviour
     {
-        
+        [Header("References")]
+        [SerializeField] private Transform _orientation;
+        [SerializeField] private Transform _player;
+        [SerializeField] private Transform _playerObject;
+        [SerializeField] private Rigidbody _rb;
+        [SerializeField] private PlayerInputReceiver _playerInputReceiver;
+
+        public float rotationSpeed;
+
+        public Transform combatLookAt;
+
+        public CameraStyle currentStyle;
+
+        public enum CameraStyle
+        {
+            Basic,
+            Combat
+        }
+
+        void Update()
+        {
+            RotatePlayer(currentStyle);
+        }
+
+        private void RotatePlayer(CameraStyle style)
+        {
+            if (style.Equals(CameraStyle.Basic))
+            {
+                Vector3 viewDirection = _player.position - new Vector3(transform.position.x, _player.position.y, transform.position.z);
+                _orientation.forward = viewDirection.normalized;
+
+                Vector3 inputDirection = _orientation.forward * _playerInputReceiver.move.y + _orientation.right * _playerInputReceiver.move.x;
+
+                if (inputDirection != Vector3.zero)
+                {
+                    _playerObject.forward = Vector3.Slerp(_playerObject.forward, inputDirection.normalized, Time.deltaTime * rotationSpeed);
+                }
+            }
+            else if (style.Equals(CameraStyle.Combat))
+            {
+                Vector3 combatDirection = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+                _orientation.forward = combatDirection.normalized;
+
+                _playerObject.forward = combatDirection.normalized;
+            }
+
+        }
     }
 }
