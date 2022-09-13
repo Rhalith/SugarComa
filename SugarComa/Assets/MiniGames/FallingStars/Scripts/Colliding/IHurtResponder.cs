4fe14e04bef8096cdc87f6c108002c3916b54c28
HFurkanBirel <hidayetfurkanbirel@gmail.com>
2022-09-13 22:32:36 +0300

    Revert "Merge branch 'FallingStars' into networkingWithDev"
    This reverts commit c5750f010e72f368c6da9b292fed7fda073f65f5, reversing
    changes made to 47e3a268f34cc7f2a5f6688179ba175fc413d7c4.

--- SugarComa/Assets/MiniGames/FallingStars/Scripts/Colliding/IHurtResponder.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IHurtResponder
-{
-    public bool CheckHit(HitData data);
-    public void Response(HitData data);
-}
