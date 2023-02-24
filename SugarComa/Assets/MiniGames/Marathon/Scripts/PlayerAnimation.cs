f238395dacca7e24a910137dea9b6c59eba2aa3e
Rhalith <81980850+Rhalith@users.noreply.github.com>
2023-02-24 03:20:44 +0300

    finished

--- SugarComa/Assets/MiniGames/Marathon/Scripts/PlayerAnimation.cs
+++ SugarComa/Assets/MiniGames/Marathon/Scripts/PlayerAnimation.cs
@@ -1,3 +1,4 @@
+using Assets.MainBoard.Scripts.Player.Movement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     public bool IsRunning;
+
+    public Animator Animator { get => _animator;}
 
     private void RunSet(int i)
     {
@@ -20,5 +23,33 @@ public class PlayerAnimation : MonoBehaviour
     public void StopRunning()
     {
         RunSet(0);
+    }
+
+    public void ChangeAnimatorSpeed(float speed)
+    {
+        switch (speed)
+        {
+            case float i when i > 0f && i < 30f:
+                _animator.speed = 0.4f;
+                break;
+            case float i when i > 30f && i < 65f:
+                _animator.speed = 0.5f;
+                break;
+            case float i when i > 65f && i < 100f:
+                _animator.speed = 0.8f;
+                break;
+            case float i when i > 100f && i < 125f:
+                _animator.speed = 1f;
+                break;
+            case float i when i > 125f && i < 150f:
+                _animator.speed = 1.2f;
+                break;
+            case float i when i > 150f && i < 200f:
+                _animator.speed = 1.4f;
+                break;
+            default:
+                _animator.speed = 1f;
+                break;
+        }
     }
 }
--- SugarComa/Assets/MiniGames/Marathon/Scripts/PlayerMovement.cs
+++ SugarComa/Assets/MiniGames/Marathon/Scripts/PlayerMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerAnimation _playerAnimation;
+    [SerializeField] private float _speed;
+    private bool _isClicking;
+    private void Start()
     {
-        
+        _playerAnimation = gameObject.GetComponent<PlayerAnimation>();
+    }
+    private void FixedUpdate()
+    {
+        MovePlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MovePlayer()
     {
-        
+        SlowDownPlayer();
+        _playerAnimation.ChangeAnimatorSpeed(_speed);
+        gameObject.transform.position += new Vector3(0,0,0.1f * _speed * Time.deltaTime);
+    }
+    private void SlowDownPlayer()
+    {
+        _speed--;
+        if (_speed < 0)
+        {
+            _playerAnimation.StopRunning();
+            _speed = 0;
+        }
+    }
+    private void SpeedUpPlayer()
+    {
+        _isClicking = true;
+        _playerAnimation.StartRunning();
+        if(_speed < 200f)_speed+=10;
+    }
+    public void OnSpeedUp()
+    {
+        SpeedUpPlayer();
     }
 }
